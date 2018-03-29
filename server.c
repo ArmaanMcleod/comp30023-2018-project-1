7eea0943cf76c58cd7123a6374dee776b2bbcebc
OpticGenius <opticgenius@hotmail.com>
2018-03-30 00:18:15 +1100

    Started assignment

---
+++ server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unisd.h>
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
